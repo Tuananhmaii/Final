caf5e43396d6648caa260dcaec5d4e2a45dd78da
Tuan Anh <maitrantuananh4802@gmail.com>
2023-07-22 14:44:44 +0700

    Update Order data table script

--- RopinStoreWeb/wwwroot/js/order.js
+++ RopinStoreWeb/wwwroot/js/order.js
@@ -1,47 +1,52 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    var url = window.location.search;
-    if (url.includes("inprocess")) {
-        loadDataTable("inprocess");
-    }
-    else {
-        if (url.includes("completed")) {
-            loadDataTable("completed");
-        }
-        else {
-            if (url.includes("pending")) {
-                loadDataTable("pending");
-            }
-            else {
-                if (url.includes("approved")) {
-                    loadDataTable("approved");
-                }
-                else {
-                    if (url.includes("cancelled")) {
-                        loadDataTable("cancelled");
-                    }
-                    else {
-                        loadDataTable("all");
-                    }
-                }
-            }
-        }
-    }
+    loadDataTable();
 });
+
+//function loadDataTable() {
+//    dataTable = $('#tblData').DataTable({
+//        "ajax": {
+//            "url": "/Admin/Order/GetAll"
+//        },
+//        "columns": [
+//            { "data": "id", "width": "5%" },
+//            { "data": "name", "width": "15%" },
+//            { "data": "phoneNumber", "width": "10%" },
+//                        { "data": "applicationUser.email", sDefaultContent: '', "width": "15%" },
+//            { "data": "orderStatus", "width": "15%" },
+//            { "data": "paymentType", "width": "10%" },
+//            { "data": "orderTotal", "width": "10%" },
+//            {
+//                "data": "id",
+//                "render": function (data) {
+//                    return `
+//                        <div class="w-75 btn-group" role="group">
+//                        <a href="/Admin/Order/Details?orderId=${data}"
+//                        class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
+                      
+//					</div>
+//                        `
+//                },
+//                "width": "5%"
+//            }
+//        ]
+//    });
+//}
 
 function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url": "/Admin/Order/GetAll?status=" + status
+            "url": "/Admin/Order/GetAll"
         },
         "columns": [
             { "data": "id", "width": "5%" },
-            { "data": "name", "width": "25%" },
+            { "data": "name", "width": "20%" },
             { "data": "phoneNumber", "width": "15%" },
             { "data": "applicationUser.email", "width": "15%" },
-            { "data": "orderStatus", "width": "15%" },
-            { "data": "orderTotal", "width": "10%" },
+            { "data": "paymentType", "width": "10%" },
+            { "data": "orderStatus", "width": "10%" },
+            { "data": "totalPrice", "width": "5%" },
             {
                 "data": "id",
                 "render": function (data) {
