b908b40726d71cab793870d58fc03a820defeced
Tuan Anh <maitrantuananh4802@gmail.com>
2023-10-06 10:17:12 +0700

    Add order to profile

--- RopinStoreWeb/Areas/Admin/Controllers/OrderController.cs
+++ RopinStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -50,139 +50,13 @@ namespace RopinStoreWeb.Areas.Admin.Controllers
             TempData["success"] = "Update successfully";
             return RedirectToAction("Details", "Order", new { orderId = orderHeaderFromDb.Id });
         }
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult StartProcessing(OrderVM orderVM)
-        //{
-        //    _unitOfWork.Order.UpdateStatus(orderVM.Order.Id,SD.StatusInProcess);
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "Update status successfully";
-        //    return RedirectToAction("Details", "Order", new { orderId = orderVM.Order.Id });
-        //}
-
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult ShipOrder(OrderVM orderVM)
-        //{
-        //    var orderHeaderFromDb = _unitOfWork.Order.GetFirstOrDefault(u => u.Id == orderVM.Order.Id);
-        //    orderHeaderFromDb.TrackingNumber = orderVM.Order.TrackingNumber;
-        //    orderHeaderFromDb.Carrier = orderVM.Order.Carrier;
-        //    orderHeaderFromDb.OrderStatus = SD.StatusShipped;
-        //    orderHeaderFromDb.ShippingDate = DateTime.Now;
-        //    if(orderHeaderFromDb.PaymentStatus == SD.PaymentStatusDelayedPayment)
-        //    {
-        //        orderHeaderFromDb.PaymentDueDate = DateTime.Now.AddDays(30);
-        //    }
-        //    _unitOfWork.Order.Update(orderHeaderFromDb);
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "Ship order successfully";
-        //    return RedirectToAction("Details", "Order", new { orderId = orderVM.Order.Id });
-        //}
-
-        //[HttpPost]
-        //[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult CancelOrder(OrderVM orderVM)
-        //{
-        //    var orderHeader = _unitOfWork.Order.GetFirstOrDefault(u => u.Id == orderVM.Order.Id);
-        //    if (orderHeader.PaymentStatus == SD.PaymentStatusAprroved)
-        //    {
-        //        var options = new RefundCreateOptions
-        //        {
-        //            Reason = RefundReasons.RequestedByCustomer,
-        //            PaymentIntent = orderHeader.PaymentIntentId
-        //        };
-
-        //        var service = new RefundService();
-        //        Refund refund = service.Create(options);
-
-        //        _unitOfWork.Order.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
-        //    }
-        //    else
-        //    {
-        //        _unitOfWork.Order.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
-        //    }
-        //    _unitOfWork.Save();
-
-        //    TempData["Success"] = "Order Cancelled Successfully.";
-        //    return RedirectToAction("Details", "Order", new { orderId = orderVM.Order.Id });
-        //}
-
-        //[ActionName("Details")]
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Details_PAY_NOW(OrderVM orderVM)
-        //{
-        //    orderVM.Order = _unitOfWork.Order.GetFirstOrDefault(u => u.Id == orderVM.Order.Id, includeProperties: "ApplicationUser");
-        //    orderVM.OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == orderVM.Order.Id, includeProperties: "Product");
-
-        //    //stripe settings 
-        //    var domain = "https://localhost:44300/";
-        //    var options = new SessionCreateOptions
-        //    {
-        //        PaymentMethodTypes = new List<string>
-        //        {
-        //          "card",
-        //        },
-        //        LineItems = new List<SessionLineItemOptions>(),
-        //        Mode = "payment",
-        //        SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderId={orderVM.Order.Id}",
-        //        CancelUrl = domain + $"admin/order/details?orderId={orderVM.Order.Id}",
-        //    };
-
-        //    foreach (var item in orderVM.OrderDetail)
-        //    {
-
-        //        var sessionLineItem = new SessionLineItemOptions
-        //        {
-        //            PriceData = new SessionLineItemPriceDataOptions
-        //            {
-        //                UnitAmount = (long)(item.Price * 100),//20.00 -> 2000
-        //                Currency = "usd",
-        //                ProductData = new SessionLineItemPriceDataProductDataOptions
-        //                {
-        //                    Name = item.Product.Title
-        //                },
-
-        //            },
-        //            Quantity = item.Count,
-        //        };
-        //        options.LineItems.Add(sessionLineItem);
-
-        //    }
-
-        //    var service = new SessionService();
-        //    Session session = service.Create(options);
-        //    _unitOfWork.Order.UpdateStripePaymentId(orderVM.Order.Id, session.Id, session.PaymentIntentId);
-        //    _unitOfWork.Save();
-        //    Response.Headers.Add("Location", session.Url);
-        //    return new StatusCodeResult(303);
-        //}
-
-        //public IActionResult PaymentConfirmation(int orderHeaderid)
-        //{
-        //    Order orderHeader = _unitOfWork.Order.GetFirstOrDefault(u => u.Id == orderHeaderid);
-        //    if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
-        //    {
-        //        var service = new SessionService();
-        //        Session session = service.Get(orderHeader.SessionId);
-        //        //check the stripe status
-        //        if (session.PaymentStatus.ToLower() == "paid")
-        //        {
-        //            _unitOfWork.Order.UpdateStatus(orderHeaderid, orderHeader.OrderStatus, SD.PaymentStatusAprroved);
-        //            _unitOfWork.Save();
-        //        }
-        //    }
-        //    return View(orderHeaderid);
-        //}
-
+        
         #region API CALL
         [HttpGet]
         public IActionResult GetAll(string status)
         {
             IEnumerable<Order> orderHeaders;
-            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            if (/*User.IsInRole(SD.Role_Admin) || */User.IsInRole(SD.Role_Employee))
             {
                 orderHeaders = _unitOfWork.Order.GetAll(includeProperties: "ApplicationUser");
             }
@@ -192,28 +66,6 @@ namespace RopinStoreWeb.Areas.Admin.Controllers
                 var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
                 orderHeaders = _unitOfWork.Order.GetAll(u=>u.ApplicationUserId == claim.Value,includeProperties: "ApplicationUser");
             }
-
-            //switch (status)
-            //{
-            //    case "pending":
-            //        orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusPending);
-            //        break;
-            //    case "inprocess":
-            //        orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
-            //        break;
-            //    case "completed":
-            //        orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
-            //        break;
-            //    case "approved":
-            //        orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
-            //        break;
-            //    case "cancelled":
-            //        orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusCancelled);
-            //        break;
-            //    default:
-            //        break;
-            //}
-
 
             return Json(new { data = orderHeaders });
         }
--- RopinStoreWeb/wwwroot/js/order.js
+++ RopinStoreWeb/wwwroot/js/order.js
@@ -47,19 +47,28 @@ function loadDataTable(status) {
             { "data": "paymentType", "width": "10%" },
             { "data": "orderStatus", "width": "10%" },
             { "data": "totalPrice", "width": "5%" },
-            {
-                "data": "id",
-                "render": function (data) {
-                    return `
-                        <div class="w-75 btn-group" role="group">
-                        <a href="/Admin/Order/Details?orderId=${data}"
-                        class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
-                      
-					</div>
-                        `
-                },
-                "width": "5%"
-            }
         ]
+    });
+
+    $('#tblData tbody').on('click', 'td', function () {
+        // Get the data in the first column of the clicked row
+        var rowData = dataTable.row($(this).closest('tr')).data();
+
+        // Check if rowData is not undefined
+        if (typeof rowData !== 'undefined') {
+            // Navigate to the specified URL using the data from the first column
+            window.location.href = `/Admin/Order/Details?orderId=${rowData.id}`;
+        }
+    });
+
+    $('#tblData tbody').on('mouseenter', 'tr', function () {
+        $(this).addClass('hovered');
+    }).on('mouseleave', 'tr', function () {
+        $(this).removeClass('hovered');
+    });
+
+    // Attach a click event to all cells in the table (same as previous code)
+    $('#tblData tbody').on('click', 'td', function () {
+        // ...
     });
 }
